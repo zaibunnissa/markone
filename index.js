7cdc4f96a3a7ce4414c23963423d21b849393c73
zaibunnissa <12zaib.h@gmail.com>
2023-01-18 23:22:31 +0530

    final update

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var readlineSync = require('readline-sync');
 var userName = readlineSync.question("Hello, May I know your name? ");
+console.log("                                                 ");
+
+console.log("**************************************************");
+
 console.log("welcome " + userName + " to Do you know about the NEOG camp!");
 console.log("**************************************************");
+console.log("                                                 ")
 console.log("Here each correct answer will fetch you +2 score, lets begin the quiz now. All the best!");
+console.log("                                                 ")
+
 var score = 0;
 function play(question, answer) {
   var userAnswer = readlineSync.question(question);
@@ -16,24 +23,24 @@ function play(question, answer) {
   console.log("---------------------------------")
 }
 var questions = [{
-  question: "Who is hosting the NEOG camp? ",
+  question: "1.Who is hosting the NEOG camp? ",
   answer: "Tanay pratap"
 },
 {
-  question: "how many levels does the camp have? ",
+  question: "2.how many levels does the camp have? ",
   answer: "2",
 },
 {
-  question: "The level zero is for absolute beginners![yes/no]",
+  question: "3.The level zero is for absolute beginners![yes/no]",
   answer: "yes"
 },
 {
-  question: "The level one of NEOG camp is free of cost and open to all.[True/false] ",
+  question: "4.The level one of NEOG camp is free of cost and open to all.[True/false] ",
   answer: "false"
 },
 
 {
-  question: "There is a developer community which helps to clear doubts regarding NEOG camp and related things, what is the name of the platform? ",
+  question: "5.There is a developer community which helps to clear doubts regarding NEOG camp and related things, what is the name of the platform? ",
   answer: "Discord"
 }]
 for (var i = 0; i < questions.length; i++) {
@@ -49,11 +56,12 @@ var highScore = [{
 },
 {
   name: "Neha",
-  score: "8"
+  score: "10"
 }]
 for (var i = 0; i < highScore.length; i++) {
   var highScorer = highScore[i];
   console.log(highScorer.name);
   console.log(highScorer.score);
 }
-console.log("If you have beaten the high score send screenshot");
+console.log("If you have beaten the high score share screenshot, we will update the High scores");
+console.log("Thank you");
